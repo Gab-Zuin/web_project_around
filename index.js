2db0630106bf9f28dc307cc23d07f7248a1b07b4
Gabs <zuni.gabi.ram@gmail.com>
2024-09-09 03:28:46 -0600

    Se agrega cuadro emergente y se da funciones JS

---
+++ index.js
@@ -0,0 +1,24 @@
+const profileButton = document.querySelector(".profile__edit-button");
+const popupProfile = document.querySelector(".popup");
+const popupClose = document.querySelector(".popup__close");
+
+const profileName = document.querySelector(".profile__name");
+const profileProfession = document.querySelector(".profile__profession");
+const inputName = document.querySelector("#input_name");
+const inputAbout = document.querySelector("#input_about");
+const inputSubmit = document.querySelector(".form__submit");
+
+profileButton.addEventListener("click", function () {
+  popupProfile.classList.add("popup_opened");
+});
+
+popupClose.removeEventListener("click", function () {
+  popupClose.classList.remove("popup_opened");
+});
+
+formProfile.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileProfession.textContent = inputAbout.value;
+  popupClose();
+});
