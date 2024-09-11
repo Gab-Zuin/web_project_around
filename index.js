c5da1c92bab8633f427fda42aa84a708405c3104
Gabs <zuni.gabi.ram@gmail.com>
2024-09-11 00:01:28 -0600

    Se completan mediaquery popup

--- index.js
+++ index.js
@@ -7,18 +7,23 @@ const profileProfession = document.querySelector(".profile__profession");
 const inputName = document.querySelector("#input_name");
 const inputAbout = document.querySelector("#input_about");
 const inputSubmit = document.querySelector(".form__submit");
+const formProfile = document.querySelector("#form__profile");
 
 profileButton.addEventListener("click", function () {
   popupProfile.classList.add("popup_opened");
 });
 
 popupClose.removeEventListener("click", function () {
-  popupClose.classList.remove("popup_opened");
+  closePopup();
 });
 
 formProfile.addEventListener("submit", function (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileProfession.textContent = inputAbout.value;
-  popupClose();
+  closePopup();
 });
+
+function closePopup() {
+  popupProfile.classList.remove("popup_opened");
+}
