00500c2d6f224875be0294ebc0c7f80a6cddad7f
Gabs <zuni.gabi.ram@gmail.com>
2025-03-21 15:34:12 -0600

    Se agrega la clase User Info

--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,32 @@
 import Card from "./Card.js";
 import FormValidator from "./FormValidator.js";
-import { openPopup, closePopup, validationsSettings } from "./utils.js";
-
-// Declaración de variables
-const profileButton = document.querySelector(".profile__edit-button");
-const popupProfile = document.querySelector("#popup-edit-profile");
-const popupCloseProfile = popupProfile.querySelector(".popup__close");
-const profileName = document.querySelector(".profile__name");
-const profileProfession = document.querySelector(".profile__profession");
-const inputName = document.querySelector("#input_name");
-const inputAbout = document.querySelector("#input_about");
-const inputSubmit = document.querySelector(".form__submit");
-const formProfile = document.querySelector("#form__profile");
-const cardTemplate = document.querySelector(".card__template").content;
-const container = document.querySelector(".container");
-const addButton = document.querySelector(".profile__add-button");
-const popupForm = document.querySelector("#popup-add-card");
-const formTitle = document.querySelector("#input_title");
-const formLink = document.querySelector("#input_link");
-const formAddCard = document.querySelector("#popup-form-card");
-const popupCloseCard = popupForm.querySelector(".popup__close");
-const popupImage = document.querySelector("#popup-image");
-const closePopupImage = popupImage.querySelector(".popup__close");
-const popupImagePhoto = popupImage.querySelector(".popup__image_expand");
-const popupImageTitle = popupImage.querySelector(".popup__image_title");
-
-const initialCards = [
-  {
-    name: "Valle de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-  },
-  {
-    name: "Montañas Calvas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-  },
-  {
-    name: "Parque Nacional de la Vanoise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-  },
-];
+import {
+  openPopup,
+  closePopup,
+  validationsSettings,
+  profileButton,
+  popupProfile,
+  popupCloseProfile,
+  profileName,
+  profileProfession,
+  inputName,
+  inputAbout,
+  inputSubmit,
+  formProfile,
+  cardTemplate,
+  container,
+  addButton,
+  popupForm,
+  formTitle,
+  formLink,
+  formAddCard,
+  popupCloseCard,
+  popupImage,
+  closePopupImage,
+  popupImagePhoto,
+  popupImageTitle,
+  initialCards,
+} from "./utils.js";
 
 // Funciones relacionadas con las tarjetas
 function cardCreate(card) {
--- scripts/utils.js
+++ scripts/utils.js
@@ -3,6 +3,56 @@
 Reutilización: Al tener los addEventListener en un archivo separado, puedes reutilizarlos en diferentes partes de tu aplicación sin duplicar código.
 Mantenimiento: Facilita el mantenimiento del código, ya que puedes encontrar y actualizar la lógica de eventos en un solo lugar. */
 
+// Declaración de variables
+export const profileButton = document.querySelector(".profile__edit-button");
+export const popupProfile = document.querySelector("#popup-edit-profile");
+export const popupCloseProfile = popupProfile.querySelector(".popup__close");
+export const profileName = document.querySelector(".profile__name");
+export const profileProfession = document.querySelector(".profile__profession");
+export const inputName = document.querySelector("#input_name");
+export const inputAbout = document.querySelector("#input_about");
+export const inputSubmit = document.querySelector(".form__submit");
+export const formProfile = document.querySelector("#form__profile");
+export const cardTemplate = document.querySelector(".card__template").content;
+export const container = document.querySelector(".container");
+export const addButton = document.querySelector(".profile__add-button");
+export const popupForm = document.querySelector("#popup-add-card");
+export const formTitle = document.querySelector("#input_title");
+export const formLink = document.querySelector("#input_link");
+export const formAddCard = document.querySelector("#popup-form-card");
+export const popupCloseCard = popupForm.querySelector(".popup__close");
+export const popupImage = document.querySelector("#popup-image");
+export const closePopupImage = popupImage.querySelector(".popup__close");
+export const popupImagePhoto = popupImage.querySelector(".popup__image_expand");
+export const popupImageTitle = popupImage.querySelector(".popup__image_title");
+
+export const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
 export const validationsSettings = {
   formSelector: ".form",
   inputSelector: ".form__input",
